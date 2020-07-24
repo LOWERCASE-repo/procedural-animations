3d9f004ba65383b41104d013c78601c2d12fb108
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-07-23 21:52:38 -0400

    fkk it im saving drag coefficients for after uni

--- MotionTester.cs
+++ MotionTester.cs
@@ -6,39 +6,26 @@ class MotionTester : MonoBehaviour {
 	[SerializeField]
 	Rigidbody2D body;
 	[SerializeField]
-	float speed;
-	[SerializeField]
-	float jumpHeight, bestHeight, jumpForce;
+	float speed, jumpForce;
+	// float jumpHeight;
 	
 	void Start() {
 		body.drag = speed / body.mass;
-		// jumpForce = Mathf.Sqrt(2f * Physics2D.gravity.magnitude * jumpHeight * body.mass);
-		// jumpForce = 2f * Physics2D.gravity.magnitude * jumpHeight * body.mass * body.drag;
+		// jumpForce = Mathf.Sqrt(2f * Physics2D.gravity.magnitude * jumpHeight);
 		
-		/*
-		gravity = 10
-		height = 2
-		mass = 4
-		drag = 2.5
-		velocity = ~10.2
-		
-		dragForceMagnitude = velocity.magnitude * drag; // The variable you're talking about
-		dragForceVector = dragForceMagnitude * -velocity.normalized;
-		
-		vel = vel.add(acc);
-    vel = vel.mult(model.drag);
-		
-		velocity = velocity * ( 1 - deltaTime * drag);
-		*/
 	}
+	
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) body.velocity = new Vector2(0f, jumpForce);
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			body.velocity = new Vector2(body.velocity.x, jumpForce);
+		}
 	}
+	
 	void FixedUpdate() {
 		Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+		if (dir.x == 0) { dir = new Vector2(-body.velocity.x, dir.y); };
 		Vector2 force = dir.normalized * speed * speed;
 		body.AddForce(force);
-		if (body.position.y > bestHeight) bestHeight = body.position.y;
-		// Debug.Log(body.velocity.magnitude + " " + body.position.y);
+		Debug.Log(body.velocity.magnitude);
 	}
 }
