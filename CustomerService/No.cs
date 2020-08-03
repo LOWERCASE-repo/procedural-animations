7963ca0b97abb73b7db098e7351dfadf3c0292a4
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-08-02 22:21:04 -0400

    i dont get paid for this

--- CustomerService/No.cs
+++ CustomerService/No.cs
@@ -4,40 +4,48 @@ using UnityEngine;
 
 public class No : MonoBehaviour
 {
-
-    public float speed;
-    public float jumpPower;
-    public bool canJump;
-
-    private Vector2 moveDirection;
-
-    public Rigidbody2D rb;
-
-
-    void Start()
-    {
-        canJump = true;
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        // if (other.gameObject.tag == "ground")
-        // {
-            canJump = true;
-        // }
-    }
-
-
-    void Update()
-    {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * speed;
-
-        if (Input.GetKey(KeyCode.Space) && canJump)
-        {
-            rb.AddForce(new Vector2(0f, 20f), ForceMode2D.Impulse);
-            canJump = false;
-        }
-    }
-
+	
+	public float speed;
+	public float jumpPower;
+	public bool canJump;
+	
+	private Vector2 moveDirection;
+	
+	public Rigidbody2D rb;
+	
+	Animator animator;
+	
+	void Start()
+	{
+		canJump = true;
+		animator = GetComponent<Animator>();
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		// if (other.gameObject.tag == "ground")
+		// {
+		canJump = true;
+		// }
+	}
+	
+	
+	void Update()
+	{
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movement * Time.deltaTime * speed;
+		Debug.Log(movement.x);
+		animator.SetFloat("WhyDoesntThisWork", movement.x);
+		if (Input.GetKey(KeyCode.Space) && canJump)
+		{
+			rb.AddForce(new Vector2(0f, 20f), ForceMode2D.Impulse);
+			canJump = false;
+		}
+		Vector3 corners[] = new Vector3[4];
+		transform.GetWorldCorners(corners);
+		Vector3 up = corners[1] - corners[0];
+		Vector3 right = corners[3] - corners[0];
+		Vector3 random = corners[0] + up * Random.value + right * Random.value;
+	}
+	
 }
